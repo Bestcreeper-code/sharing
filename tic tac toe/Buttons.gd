765556fe32f649a4ff602bd1ee291f3b1530b4d4
Bestcreeper-code <bestcreeper21@gmail.com>
2025-02-05 21:48:52 +0100

    eh

--- tic tac toe/Buttons.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-func _process(delta):
-	if Vars.win == true:
-		self.visible = false
-	else :
-		self.visible = true
--- tic tac toe/Xwon.gd
+++
@@ -1,8 +0,0 @@
-extends TextEdit
-
-
-func _process(delta):
-	if Vars.whowon == 1:
-		show()
-	else:
-		hide()
